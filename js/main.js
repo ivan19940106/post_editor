ce60ea4dbcf05b528821ec6b024c7e2f6b7ed8a4
ivan19940106 <tanoshii06@gmail.com>
2021-09-11 23:55:09 +0800

    add drag & add Template feature

--- js/main.js
+++ js/main.js
@@ -1,66 +1,71 @@
 $(document).ready(function(){
+    var wordMapping = {
+        published: 'published!!',
+        editing : 'editing!!',
+        layoutChangeCompleted : 'layout change completed!!',
+        layoutChanging : 'layout changing!!'
+    }
+
     $('.complete-button').click(function(){
         if($(this).is(":checked") === false){
             $('.text').attr('contenteditable', 'false');
             $('.text').removeClass('border-highlight');
+            $('.text').unbind('mouseenter mouseleave');
             $('.complete-button-label').text(wordMapping.published);
-            $('.icon-pen').hide();
-            $('.icon-print').show();
+            $('.complete-button-wrapper .icon-print').removeClass('hidden');
+            $('.complete-button-wrapper .icon-pen').addClass('hidden');
         } else {
+            // editing
             $('.text').attr('contenteditable', 'true');
             $('.text').addClass('border-highlight');
+            enableAddTemplate();
             $('.complete-button-label').text(wordMapping.editing);
-            $('.icon-pen').show();
-            $('.icon-print').hide();
+            $('.complete-button-wrapper .icon-print').addClass('hidden');
+            $('.complete-button-wrapper .icon-pen').removeClass('hidden');
         }
     });
 
-    var wordMapping = {
-        published: 'published!!',
-        editing : 'editing!!'
+    $('.layout-button').click(function(){
+        if($(this).is(":checked") === true){
+            enableSort();
+            $('.layout-button-label').text(wordMapping.layoutChanging);
+            $('.layout-button-wrapper .icon-pen').removeClass('hidden');
+            $('.layout-button-wrapper .icon-print').addClass('hidden');
+        } else {
+            disableSort();
+            $('.layout-button-label').text(wordMapping.layoutChangeCompleted);
+            $('.layout-button-wrapper .icon-pen').addClass('hidden');
+            $('.layout-button-wrapper .icon-print').removeClass('hidden');
+        }
+    });
+    
+    enableAddTemplate();
+
+    function enableAddTemplate() {
+        $('.text.border-highlight').hover(function(){
+            var textNode = $(this);
+            var addTemplateButton = document.createElement('i');
+            addTemplateButton.className = "icon-plus add-template-button";
+            addTemplateButton.style.display = 'none';
+            textNode.prepend(addTemplateButton);
+            $('.icon-plus.add-template-button').fadeIn();
+            // $(this).prepend('<i class="icon-plus add-template-button"></i>');
+            $('.icon-plus.add-template-button').click(function(){
+                var newTextNode = textNode.clone().text('');
+                newTextNode.insertAfter(textNode);
+            });
+        }, function(){
+            $(this).find('.icon-plus').remove();
+        });
+    }
+    
+    function enableSort(){
+        $('.text-wrapper').addClass('drag-list');
+        $( ".drag-list" ).sortable();
     }
 
-    // https://codepen.io/danielsintrao/pen/OgWJae
-    $(".drag-list").sortable({
-        axis: 'y',
-        placeholder: 'slide-placeholder',
-        revert: 150,
-        start: function (e, ui) {
-            var placeholderHeight = ui.item.outerHeight();
-            ui.placeholder.height(placeholderHeight);
-            ui.placeholder.css({
-                'margin-top': ui.item.css('margin-top'),
-                'margin-right': ui.item.css('margin-right'),
-                'margin-bottom': ui.item.css('margin-bottom'),
-                'margin-left': ui.item.css('margin-left')
-            });
-            $('<div class="slide-placeholder-animator" data-height="' + placeholderHeight + '"></div>').insertAfter(ui.placeholder);
-        },
-        change: function (event, ui) {
-            ui.placeholder.stop().height(0).animate({
-                height: ui.item.outerHeight(),
-                marginTop: parseInt(ui.item.css('margin-top')),
-                marginRight: parseInt(ui.item.css('margin-right')),
-                marginBottom: parseInt(ui.item.css('margin-bottom')),
-                marginLeft: parseInt(ui.item.css('margin-left'))
-            }, 150);
-            placeholderAnimatorHeight = parseInt($(".slide-placeholder-animator").attr("data-height"));
-
-            $(".slide-placeholder-animator").stop().height(placeholderAnimatorHeight).animate({
-                height: 0,
-                marginTop: 0,
-                marginRight: 0,
-                marginBottom: 0,
-                marginLeft: 0
-            }, 150, function () {
-                $(this).remove();
-                var placeholderHeight = ui.item.outerHeight();
-                $('<div class="slide-placeholder-animator" data-height="' + placeholderHeight + '"></div>').insertAfter(ui.placeholder);
-            });
-        },
-        stop: function (e, ui) {
-            console.log($(".drag-list drag-item").first().text());
-            $(".slide-placeholder-animator").remove();
-        },
-    });
+    function disableSort(){
+        $( ".drag-list" ).sortable('destroy');
+        $("drag-item").removeClass('ui-state-default');
+    }
 });
